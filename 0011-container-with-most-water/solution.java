60a44f7bb30e8b6f5aca4b8f596a7d4fad7e36f5
raphaelkaramagi <karamagiraphael@gmail.com>
2025-10-23 02:37:47 +0000

    Sync LeetCode submission Runtime - 5 ms (82.38%), Memory - 58.1 MB (99.92%)

--- 0011-container-with-most-water/solution.java
+++ 0011-container-with-most-water/solution.java
@@ -1,23 +1,36 @@
 class Solution {
     public int maxArea(int[] height) {
+        // PLAN
+        // 0. init maxArea =0; return maxArea;
+        // 0. Create a pointer at the far left and right
+        // 1. Calculate the area (smaller value * difference), and assign it to maxArea;
+        // 2. Compare the 2 pointers, move the smaller one inward until its larger than the other pointer
+        // 3. Calculate area again if found, assign to maxArea.
+
         int maxArea = 0;
-        int left = 0;
-        int right = height.length - 1;
 
-        while (left < right) {
-            int width = right - left;
-            int minHeight = Math.min(height[left], height[right]);
-            int area = width * minHeight;
+        int steps = 0;
+        int leftPointer = 0;
+        int rightPointer = height.length - 1;
 
-            maxArea = Math.max(maxArea, area);
+        while (steps < height.length - 1) {
+            int leftHeight = height[leftPointer];
+            int rightHeight = height[rightPointer];
+            int minHeight = Math.min(leftHeight, rightHeight);
 
-            if (height[left] < height[right]) {
-                left++;
-            } else {
-                right--;
+            maxArea = Math.max((minHeight * (rightPointer - leftPointer)), maxArea);
+
+            if (leftHeight <= rightHeight) {
+                leftPointer++;
+                steps++;
+
+            } else if (rightHeight <= leftHeight) {
+                rightPointer--;
+                steps++;
+
             }
-        }
 
+        }
         return maxArea;
     }
 }
