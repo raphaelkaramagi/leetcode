1cfe799a146363a444558a483d0bcd2a8409cc3a
raphaelkaramagi <karamagiraphael@gmail.com>
2025-10-26 04:22:59 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.1 MB (99.97%)

--- 0104-maximum-depth-of-binary-tree/solution.java
+++ 0104-maximum-depth-of-binary-tree/solution.java
@@ -15,9 +15,10 @@
  */
 class Solution {
     public int maxDepth(TreeNode root) {
+        // DFS
         if(root == null) return 0;
         int leftDepth = maxDepth(root.left);
         int rightDepth = maxDepth(root.right);
-        return 1 + Math.max(leftDepth, rightDepth);
+        return 1 + Math.max(rightDepth, leftDepth);
     }
 }
