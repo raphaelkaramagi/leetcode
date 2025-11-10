622e080ef2a770bc9ad6c06134199068264b003a
raphaelkaramagi <karamagiraphael@gmail.com>
2025-11-10 17:54:05 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.7 MB (100.00%)

---
+++ 0118-pascals-triangle/solution.py
@@ -0,0 +1,20 @@
+import math
+from typing import List
+
+class Solution:
+
+    def combinator(self, n: int, k: int) -> int:
+        return math.comb(n,k)
+
+    def generate(self, numRows: int) -> List[List[int]]:
+        output = []
+        
+        for row in range(numRows):
+            subOutput = []
+            k=0
+            while(len(subOutput) <= row):
+                subOutput.append(self.combinator(row, k))
+                k+=1
+            output.append(subOutput)
+
+        return output
