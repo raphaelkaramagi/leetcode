6bff56f2ada97b930907580b52fc68dc014de4d9
raphaelkaramagi <karamagiraphael@gmail.com>
2025-12-28 02:02:25 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18.8 MB (100.00%)

--- 0001-two-sum/solution.py
+++ 0001-two-sum/solution.py
@@ -1,15 +1,15 @@
 from typing import List
 
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # HashMap of complements
-        output = []
-        
-        complements = {}
+        def twoSum(self, nums: List[int], target: int) -> List[int]:
+            # Map of complements
+            comps = {}
+            output = []
+            for i in range(len(nums)):
+                if nums[i] in comps: 
+                    output.append(i)
+                    output.append(comps[nums[i]])
+                    return output
+                comps[target-nums[i]] = i
 
-        for i, num in enumerate(nums):
-            if num in complements:
-                return [complements[num], i]
-            complement = target - num
-            complements[complement] = i
-        return []
+            return None
