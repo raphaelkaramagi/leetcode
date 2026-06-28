a91ed5bc19a818c14f48fe1ca2c3d5687b77d3e8
raphaelkaramagi <karamagiraphael@gmail.com>
2026-06-28 17:15:53 +0000

    Sync LeetCode submission Runtime - 11 ms (77.07%), Memory - 19.2 MB (94.37%)

--- 0242-valid-anagram/solution.py
+++ 0242-valid-anagram/solution.py
@@ -1,8 +1,14 @@
-from collections import Counter
-
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-        return Counter(s) == Counter(t)
-
+        sMap = {}
+        for ch in s:
+            sMap[ch] = sMap.get(ch,0) + 1
+        tMap = {}
+        for ch in t:
+            tMap[ch] = tMap.get(ch,0) + 1
+        if sMap == tMap:
+            return True
+        return False
+        
