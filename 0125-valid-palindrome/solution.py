60f62a4dac82a4cd2850b33da5f54d8c797c0a17
raphaelkaramagi <karamagiraphael@gmail.com>
2026-06-29 02:38:16 +0000

    Sync LeetCode submission Runtime - 6 ms (84.81%), Memory - 20.2 MB (30.49%)

--- 0125-valid-palindrome/solution.py
+++ 0125-valid-palindrome/solution.py
@@ -1,18 +1,8 @@
 class Solution:
-
     def isPalindrome(self, s: str) -> bool:
+        alnumLow = "".join([char for char in s.lower() if char.isalnum()])
+        reverse = alnumLow[::-1]
+        if alnumLow == reverse:
+            return True
+        return False
 
-        #Two pointer approach to optimize space complexity, not creating new string (O(1) space, O(N) time)
-        left, right = 0, len(s)-1
-
-        while left < right:
-            while left < right and not s[left].isalnum():
-                left+=1
-            while left < right and not s[right].isalnum():
-                right-=1
-            if left < right and s[left].lower() != s[right].lower():
-                return False
-            left+=1
-            right-=1
-
-        return True
