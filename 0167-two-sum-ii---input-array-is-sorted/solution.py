08805652fd2195c370f9cb0e634479ea9cced5ae
raphaelkaramagi <karamagiraphael@gmail.com>
2026-07-05 23:38:42 +0000

    Sync LeetCode submission Runtime - 3 ms (80.22%), Memory - 20.5 MB (75.02%)

--- 0167-two-sum-ii---input-array-is-sorted/solution.py
+++ 0167-two-sum-ii---input-array-is-sorted/solution.py
@@ -1,18 +1,18 @@
-from typing import List
-
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        output = []
-        # Two Pointers
+        # Start with right, if target-right < left: decrease right 
+        
         left = 0
-        right = len(numbers) -1
-        while(left < right):
-            sum = numbers[left] + numbers[right]
-            if sum == target:
-                output.append(left+1)
-                output.append(right+1)
-                break
-            elif sum > target: right -=1
-            else: left +=1
+        right = len(numbers)-1
 
-        return output
+        while left < right:
+            if numbers[left] > target - numbers[right]:
+                right-=1
+            elif numbers[left] == target - numbers[right]:
+                break
+            else:
+                left+=1
+
+        solution = [left+1, right+1]
+        return solution
+        
