cf23bc86c0069fd8217387fffe3c8afb6692c830
raphaelkaramagi <karamagiraphael@gmail.com>
2026-09-06 01:50:12 +0000

    Sync LeetCode submission Runtime - 1 ms (62.62%), Memory - 27.7 MB (80.58%)

---
+++ 0143-reorder-list/solution.py
@@ -0,0 +1,60 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        # Going left, right, left +1, right-1 etc.
+        # Find middle and reverse second half and splice 
+
+        # To find middle, use tortoise and hare, when fast reaches end, 
+        fast = head
+        slow = head
+
+        # Finding midpoint
+        while(fast is not None):
+            if fast.next is None:
+                break
+            elif fast.next.next is None:
+                break
+            fast = fast.next.next
+            slow = slow.next
+
+        curr = slow.next
+        slow.next = None
+        prev = None
+        secondStart = None
+
+        # Reverse second half
+        while curr is not None:
+            temp = curr.next
+            curr.next = prev
+            if temp is None:
+                secondStart = curr
+            prev = curr
+            curr = temp
+
+        [1,2]
+        [3,4]
+
+        [1,2,3]
+        [4,5]
+
+        # Splicing together
+        firstStart = head
+        while firstStart is not None:
+            temp = firstStart.next
+            firstStart.next = secondStart
+            if secondStart is not None:
+                temp2 = secondStart.next
+                secondStart.next = temp
+                secondStart = temp2
+            firstStart = temp
+            
+
+
+
